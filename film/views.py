882974047d88dfe0932159be65a4b5eb7b2be4a5
kagolo <84382333+kagolo@users.noreply.github.com>
2021-10-30 15:11:03 -0700

    Added Views,urls and corresponding template for login

--- film/views.py
+++ film/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User,auth
 from.models import(Movies)
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm 
+
 
 from .form import NewUserForm
 from django.contrib.auth import login
@@ -76,19 +79,35 @@ def register_request(request):
 	form = NewUserForm()
 	return render (request=request, template_name="register.html", context={"register_form":form})
 
-def login_request(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = auth.authenticate(username=username,password=password)
-        if user is not None:
-            auth.login(request,user)
-            return redirect('/')
-        else:
-            messages.info(request,'Invalid Credentials')
-            return redirect('login')
+# def login_request(request):
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         password = request.POST['password']
+#         user = auth.authenticate(username=username,password=password)
+#         if user is not None:
+#             auth.login(request,user)
+#             return redirect('/')
+#         else:
+#             messages.info(request,'Invalid Credentials')
+#             return redirect('login')
     
-    else:
-        return render (request,'login.html')  
+#     else:
+#         return render (request,'login.html')  
 
-      
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("movie")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="login.html", context={"login_form":form})
