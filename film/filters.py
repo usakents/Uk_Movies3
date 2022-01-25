be949463c13526ffa58550c358d2260ca0dbfcfa
kagolo <84382333+kagolo@users.noreply.github.com>
2022-01-25 11:18:37 -0800

    Added grouped link buttons in idex page

--- film/filters.py
+++ film/filters.py
@@ -6,16 +6,16 @@ from .models import Movies
 from .models import  Series
 
 class Movie_filter(django_filters.FilterSet):
-    start_date = DateFilter(field_name="movie_release_date",lookup_expr='gte')
-    end_date = DateFilter(field_name="movie_release_date",lookup_expr='lte')
+    # start_date = DateFilter(field_name="movie_release_date",lookup_expr='gte')
+    # end_date = DateFilter(field_name="movie_release_date",lookup_expr='lte')
     movie_title = django_filters.CharFilter(lookup_expr='icontains')
     movie_VJ = django_filters.CharFilter(lookup_expr='icontains')
     movie_general = django_filters.CharFilter(lookup_expr='icontains')
-    movie_title = CharFilter(field_name="movie_title", lookup_expr='icontains')
+    # movie_title = CharFilter(field_name="movie_title", lookup_expr='icontains')
     class Meta:
         model = Movies
-       
-        fields = ['movie_title','movie_general','movie_VJ','movie_cost','movie_status1','movie_release_date']
+        fields = ['movie_title','movie_general','movie_VJ','movie_status1']
+        # fields = ['movie_title','movie_general','movie_VJ','movie_cost','movie_status1','movie_release_date']
         # exclude =['movie_general']
        
 class Movie_title_filter(django_filters.FilterSet):
